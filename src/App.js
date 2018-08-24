81506e16d842e156a737835c28bfadfa95094f26
Akhil <akhil9tiet@gmail.com>
2018-08-24 11:14:54 -0700

    local storage made and got data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const products = [
+  {name: 'iPad',
+   price: 200
+  },
+  {name: 'iPhone',
+   price: 650
+  }
+];
+
+localStorage.setItem('products', JSON.stringify(products));
+
 class App extends Component {
+
+  //Component will mount is mostly for getting data before your component loads
+  componentWillMount(){
+    const products = JSON.parse(localStorage.getItem('products'));
+
+    console.log(products);
+  } 
+
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+        <h1>My Products App</h1>
       </div>
     );
   }
